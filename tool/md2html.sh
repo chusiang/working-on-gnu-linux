f180709d878376985d6f0b614efac5bbe0ba85fe
chusiang <chusiang.lai@gmail.com>
2013-09-25 09:51:07 +0800

    add <a> tag option.

--- tool/md2html.sh
+++ tool/md2html.sh
@@ -2,11 +2,18 @@
 
 # Filename: 2html.sh
 
+# remove <p> tags.
 sed -i 's/<p>//g' $@
 sed -i 's/<\/p>//g' $@
 
+# remove <pre> tags.
 sed -i 's/<pre>//g' $@
 sed -i 's/<\/pre>//g' $@
 
+# replace <code> to <blockquote> tags.
 sed -i 's/<code>/<blockquote>/g' $@
 sed -i 's/<\/code>/<\/blockquote>/g' $@
+
+# add target="_blank" option into <a> tags.
+sed -i 's/<a\ href/<a\ target="_blank"\ href/g' $@
+
