34b031dd2af2c4be4292a9f3a36a805b49f10121
chusiang <chusiang.lai@gmail.com>
2013-10-18 15:48:30 +0800

    add markdown_py and xclip.

--- tool/md2html.sh
+++ tool/md2html.sh
@@ -2,18 +2,24 @@
 
 # Filename: 2html.sh
 
+# switch format of  markdown to html.
+markdown_py $1 > 123.html
+
 # remove <p> tags.
-sed -i 's/<p>//g' $@
-sed -i 's/<\/p>//g' $@
+sed -i 's/<p>//g' 123.html
+sed -i 's/<\/p>//g' 123.html
 
 # remove <pre> tags.
-sed -i 's/<pre>//g' $@
-sed -i 's/<\/pre>//g' $@
+sed -i 's/<pre>//g' 123.html
+sed -i 's/<\/pre>//g' 123.html
 
 # replace <code> to <blockquote> tags.
-sed -i 's/<code>/<blockquote>/g' $@
-sed -i 's/<\/code>/<\/blockquote>/g' $@
+sed -i 's/<code>/<blockquote>/g' 123.html
+sed -i 's/<\/code>/<\/blockquote>/g' 123.html
 
 # add target="_blank" option into <a> tags.
-sed -i 's/<a\ href/<a\ target="_blank"\ href/g' $@
+sed -i 's/<a\ href/<a\ target="_blank"\ href/g' 123.html
+
+# copy html to 'clipboard'.
+cat 123.html | xclip
 
